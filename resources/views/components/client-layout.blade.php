d116d596fcd9514a65dfeca973431c0bbf51d9c8
MohammedPrince <mohammed.beng@gmail.com>
2024-01-05 18:34:38 +0400

    commit

--- resources/views/components/client-layout.blade.php
+++ resources/views/components/client-layout.blade.php
@@ -24,7 +24,7 @@
 	<!--[if lt IE 9]>
 	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
+	<![endif][]-->
 
 	{{-- <script
 	nonce="1e3b7c67-d2f3-4362-a5da-f9977a2f5706">(function (w, d) 
