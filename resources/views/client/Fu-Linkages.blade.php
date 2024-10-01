ee0ba38bdd705e52caf0e8dcd18f7565f769ed96
MohammedPrince <mohammed.beng@gmail.com>
2024-10-01 14:04:04 +0400

    Code Updates. make table scrollable

--- resources/views/client/Fu-Linkages.blade.php
+++ resources/views/client/Fu-Linkages.blade.php
@@ -2,92 +2,92 @@
 @section('main-client-content')
     <div class="site-breadcrumb">
         <div class="container">
-            <a href="{{route('Home')}}"><i class="fa fa-home"></i> Home</a> <i class="fa fa-angle-right"></i>
+            <a href="{{ route('Home') }}"><i class="fa fa-home"></i> Home</a> <i class="fa fa-angle-right"></i>
             <a href="#">About Future Univesity</a><i class="fa fa-angle-right"></i>
             <span style="color: #fb8b24;">FU Linkages</span>
         </div>
     </div>
-
     <section class="about-section">
 
-		<div class="container">
+        <div class="container">
 
-			<div class="post-item post-details" style="padding-top: 0px;">
-				<div class="section-title ">
-					<div class="post-content">
-						<p></p>
+            <div class="post-item post-details" style="padding-top: 0px;">
+                <div class="section-title ">
+                    <div class="post-content">
+                        <p></p>
 
-						<p>
-						<blockquote class="text-center" style="background-color: whitesmoke;">
-							<span style="font-size: 18px;">
-								The University promotes a global outlook through partnerships and linkages with foreign
-								universities
-								and institutions including student exchanges, staff exchange, joint research projects
-								and sharing of knowledge.
-							</span>
-						</blockquote>
-						</p>
+                        <p>
+                        <blockquote class="text-center" style="background-color: whitesmoke;">
+                            <span style="font-size: 18px;">
+                                The University promotes a global outlook through partnerships and linkages with foreign
+                                universities
+                                and institutions including student exchanges, staff exchange, joint research projects
+                                and sharing of knowledge.
+                            </span>
+                        </blockquote>
+                        </p>
 
-						<div class="hr-sect new4"><span>International Linkages</span></div>
+                        <div class="hr-sect new4"><span>International Linkages</span></div>
+
+                        <div class="table-container">
+                            <table class="table table-bordered table-hover">
+                                <tbody>
+                                    <tr>
+                                        <td>Multimedia University, Malaysia</td>
+                                        <td>Nottingham University, Malaysia Campus</td>
+                                        <td>University of Juba (UOJ), South Sudan</td>
+                                        <td>Institut Teknologi Bandung (ITB), Bandung-Indonesia</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Universita Telematica Internazionale UNINETTUNO, Rome</td>
+                                        <td>Universitas Paramadina, Indonesia</td>
+                                        <td>Universidade De Vigo, Spain</td>
+                                        <td>Cankiri University, Turkey</td>
+                                    </tr>
+                                    <tr>
+                                        <td>KANGNAM University (KNU), Korea</td>
+                                        <td>Technical University of SOFIA, Republic of Bulgaria</td>
+                                        <td>Universita' di Venezia (IUAV), Venice Italy;</td>
+                                        <td>Addis Ababa (AAU), Ethiopia</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Universitas Trisakti, Indonesia</td>
+                                        <td>Islamic University of As-Syafiiyad</td>
+                                        <td>University of Indonesia</td>
+                                        <td></td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
 
 
-						<div>
-						<table class="table table-bordered table-hover">
-								<tbody>
-									<tr>
-										<td>Multimedia University, Malaysia</td>
-										<td>Nottingham University, Malaysia Campus</td>
-										<td>University of Juba (UOJ), South Sudan</td>
-										<td>Institut Teknologi Bandung (ITB), Bandung-Indonesia</td>
-									</tr>
-									<tr>
-										<td>Universita Telematica Internazionale UNINETTUNO, Rome</td>
-										<td>Universitas Paramadina, Indonesia</td>
-										<td>Universidade De Vigo, Spain</td>
-										<td>Cankiri University, Turkey</td>
-									</tr>
-									<tr>
-										<td>KANGNAM University (KNU), Korea</td>
-										<td>Technical University of SOFIA, Republic of Bulgaria</td>
-										<td>Universita' di Venezia (IUAV), Venice Italy;</td>
-										<td>Addis Ababa (AAU), Ethiopia</td>
-									</tr>
-									<tr>
-										<td>Universitas Trisakti, Indonesia</td>
-										<td>Islamic University of As-Syafiiyad</td>
-										<td>University of Indonesia</td>
-										<td></td>
-									</tr>
-								</tbody>
-							</table>
-						</div>
 
-	
+                        <div class="hr-sect new4"><span>Local Linkages</span></div>
+                        <div class="table-container">
+                            <table class="table table-bordered table-hover">
+                                <tbody>
+                                    <tr>
+                                        <td>Sudan Airways (SudanAir)</td>
+                                        <td>Arab Academy of Sciences (AAS)</td>
+                                        <td>National Information Center (NIC)</td>
+                                        <td>GIAD Company</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Sudan-Turk Vocational &amp; Technical Education Centre for Trainers</td>
+                                        <td>GIAD Company</td>
+                                        <td>Al Haj Yousif Vocational Training and Business Skill Centre (HYVTC)</td>
+                                        <td>Red Sea University, Port Sudan</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <p>
+                            <hr class="new4">
+                        </p>
 
-						<div class="hr-sect new4"><span>Local Linkages</span></div>
-						<div>
-							<table class="table table-bordered table-hover">
-								<tbody>
-									<tr>
-										<td>Sudan Airways (SudanAir)</td>
-										<td>Arab Academy of Sciences (AAS)</td>
-										<td>National Information Center (NIC)</td>
-										<td>GIAD Company</td>
-									</tr>
-									<tr>
-										<td>Sudan-Turk Vocational &amp; Technical Education Centre for Trainers</td>
-										<td>GIAD Company</td>
-										<td>Al Haj Yousif Vocational Training and Business Skill Centre (HYVTC)</td>
-										<td>Red Sea University, Port Sudan</td>
-									</tr>
-								</tbody>
-							</table>
-						</div>
-						<p><hr class="new4"></p>
-						
-					</div>
-				</div>
-			</div>
-		</div>
-	</section>
-    @endsection
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
